c7359d2157250a7dfe60e3bc58264ab9d7a9394d
Scheunemannn <99615216+Scheunemannn@users.noreply.github.com>
2023-03-20 20:25:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let a = document.getElementById('1')
+let b = document.getElementById('2')
+let c = document.getElementById('3')
+let d = document.getElementById('4')
+
+
+
+
+
+
+
+const verificadoraCerta = () => {
+    a.style.background = "green"
+    b.style.background = "red"
+    c.style.background = "red"
+    d.style.background = "red"
+}
+
+
